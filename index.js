6eef8aa4e17b79abbd274c5b2fb74e7084827275
Phellipe <phellipecardosooo@gmail.com>
2026-06-05 14:26:04 -0300

    feat: create movie search CLI using TMDb API

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ async function searchMovies(movie) {
     const data = await response.json();
     return data.results || [];
 }
+async function getMovieDetails(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
 
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+function formatRuntime(min){
+    const hours = Math.floor( min / 60);
+    const minutes = min % 60;
+
+    if(hours === 0) return `${minutes}min`;
+    if(minutes === 0) return `${hours}h`;
+    return `${hours}h ${minutes}min`;
+}
 async function main() {
     const movie = process.argv.slice(2).join(" ");
 
@@ -24,21 +38,26 @@ async function main() {
         console.log("Filme não encontrado");
         return;
     }
-    const choices = results.slice(0, 5).map((m, index) =>({
-        name: `${m.title} (${m.release_date})`,
+    const choices = results.slice(0, 5).map((m =>({
+        name: `${m.title} (${m.release_date || "Unknown"})`,
         value: m.id
-    }));
+    })));
     const selectedMovie = await select({
     message: "Escolha um filme:",
     choices
     });
 
-    const movieData = results.find(m => m.id === selectedMovie);
+    const movieData = await getMovieDetails(selectedMovie);
 
-    console.log("\n=== Details ===");
-    console.log("Title:", movieData.title);
-    console.log("Release date:", movieData.release_date);
-    console.log("Score:", movieData.vote_average || "N/A");
-    console.log("Summary:", movieData.overview);
+    console.log("\n=== Details ===\n");
+    console.log("🎬 Title:", movieData.title);
+    console.log("📅 Release date:", movieData.release_date);
+    console.log("⭐ Score:", movieData.vote_average ? movieData.vote_average.toFixed(1) + "/10": "N/A");
+    console.log("⏱ Runtime:", formatRuntime(movieData.runtime));
+    console.log(
+        "🎭 Genres:", movieData.genres.map(g => g.name).join(", ")
+    );
+    console.log("🗒️ Summary:", movieData.overview || "No summary available.");
+    console.log("\n");
 }
 main();
